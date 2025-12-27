afaf1810ba8fd669545eadedf32e70b8089a7284
brianwaithaka <waithakabrian8@gmail.com>
2025-12-27 09:27:03 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [even for even in num_list if even % 2 == 0]
+
+    return even_numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    ammended_sentence = [sentence + "!" for sentence in sentence_list]
+    return ammended_sentence
